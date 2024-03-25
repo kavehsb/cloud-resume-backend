9f6299f39d82b7d1c92fd5a2f96ff8754ad43809
Kaveh Buenaventura <kavehsbuen@gmail.com>
2024-03-24 22:21:22 -0700

    Remove default s3 bucket and add visitor count db table

--- __main__.py
+++ __main__.py
@@ -1,10 +1,25 @@
-"""An AWS Python Pulumi program"""
-
 import pulumi
-from pulumi_aws import s3
+import pulumi_aws as aws
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
-
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+crc_visitor_count = aws.dynamodb.Table("crc-visitor-count",
+    attributes=[
+        aws.dynamodb.TableAttributeArgs(
+            name="visitorCount",
+            type="S",
+        ),
+        aws.dynamodb.TableAttributeArgs(
+            name="id",
+            type="S",
+        ),
+    ],
+    billing_mode="PAY_PER_REQUEST",
+    hash_key="id",
+    name="crc-visitor-count",
+    point_in_time_recovery=aws.dynamodb.TablePointInTimeRecoveryArgs(
+        enabled=False,
+    ),
+    range_key="visitorCount",
+    ttl=aws.dynamodb.TableTtlArgs(
+        attribute_name="",
+    ),
+    opts=pulumi.ResourceOptions(protect=True))
